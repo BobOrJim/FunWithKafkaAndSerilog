19456f50acfb2aaf93ba1307189296dbb256c2dc
Jimmy Nordin <jimmy.nordin.1979@gmail.com>
2021-11-12 17:30:17 +0100

    ConsoleProducerV2 added

---
+++ src/ConsoleProducerV2/EventPublisher.cs
@@ -0,0 +1,24 @@
+﻿using Confluent.Kafka;
+using System;
+using Microsoft.Extensions.Configuration;
+
+namespace ConsoleProducerV2
+{
+    public class EventPublisher
+    {
+        public void PublishEvent(string topic = "", string key = "", string value = "")
+        {
+            using (var producer = new ProducerBuilder<string, string>(new ProducerConfig { BootstrapServers = "localhost:9092" }).Build())
+            {
+                producer.Produce(topic, new Message<string, string> { Key = key, Value = value }, (deliveryReport) => 
+                    {
+                        if (deliveryReport.Error.Code != ErrorCode.NoError)
+                            {
+                                Console.WriteLine($"Failed to deliver message: {deliveryReport.Error.Reason}");
+                            }
+                    });
+                producer.Flush(TimeSpan.FromSeconds(10));
+            } //Using
+        } //method
+    } //class
+} //namespace
---
+++ src/ConsoleProducerV2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleProducerV2
+{
+    public class Program
+    {
+        static void Main()
+        {
+
+
+            Producer producer1 = new();
+            producer1.EventProducer();
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
